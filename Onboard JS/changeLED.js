27a8c79d4c6ebf93c16c81fe7a837851831908e1
SWorster <131273379+SWorster@users.noreply.github.com>
2023-06-09 14:14:58 -0400

    Add files via upload

---
+++ Onboard JS/changeLED.js
@@ -0,0 +1,9 @@
+misty.Debug("Color change time!");
+
+_changeColors();
+
+misty.Debug("color done!");
+
+function _changeColors() {
+    misty.ChangeLED(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+}
