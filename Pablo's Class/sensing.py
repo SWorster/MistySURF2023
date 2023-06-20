98993133ed130b1dd602b400f98ea173a981f534
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2023-06-20 11:31:44 -0400

    Update sensing.py

--- Pablo's Class/sensing.py
+++ Pablo's Class/sensing.py
@@ -1,40 +1,34 @@
 '''
 Sensing Modalities
 
-This code can be used to test all of Misty's sensory capabilities. The following is a list of events and commands we can use (filtered to exclude completion messages, various notifications, etc).
+This code can be used to test some of Misty's sensory capabilities. The following is the list of events and commands this code uses. Most of the sensory capabilities are boring and don't make sense for a demo, so I was generous with what I cut.
+
+Because I haven't done any work with audio yet, I've left that section incomplete.
 
 Proprioception
     - ActuatorPosition (arms/head)
     - DriveEncoders (treads)
     - IMU (vestibular)
-    - LocomotionCommand (change in velocity)
 
-Audio
-    - KeyPhraseRecognized (use with StartkeyPhraseRecognition)
+Audio 
+    - KeyPhraseRecognized (use with StartKeyPhraseRecognition)
     - SourceTrackDataMessage
     - SourceFocusConfigMessage
     - VoiceRecord (use with CaptureSpeech)
     
 Visual
-    - FaceRecognition (use with StartFaceDetection, StartFaceRecognition)
-    - FaceTraining (use with StartFaceTraining)
+    - FaceRecognition (use with StartFaceDetection)
     - TimeOfFlight
-    - ArTagDetection (use with StartArTagDetector)
-    - ObjectDetection (use with StartObjectDetector)
-    - WorldState
 
 Tactile
     - BumpSensor
     - TouchSensor
-
-This is a lot, so I'm cutting most of them. Most don't make sense for a demo, anyways.
 '''
 
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 import os
 import time
-import keyboard
 
 ################################################################################################
 #                                       PROPRIOCEPTION                                         #
@@ -43,47 +37,32 @@ import keyboard
 
 def _ActuatorPosition(data):
     print("\n\nActuatorPosition\n", data)
-    misty.Speak("Actuator Position data printed", speechRate=2)
 
 
 def _DriveEncoders(data):
     print("\n\nDriveEncoders\n", data)
-    misty.Speak("Drive Encoders data printed", speechRate=2)
 
 
 def _IMU(data):
     print("\n\nIMU\n", data)
-    misty.Speak("I M U data printed", speechRate=2)
-
-
-def _LocomotionCommand(data):
-    print("\n\nLocomotionCommand\n", data)
-    misty.Speak("Locomotion Command data printed", speechRate=2)
 
 
 def proprioception():
     '''
-    Misty will verbally tell us when each of these are sent.
-    I can't think of a better way to demo these.
+    I can't think of a better way to demo these than to print the data.
     '''
-    mode()
     misty.RegisterEvent("ActuatorPosition", Events.ActuatorPosition, condition=None,
-                        debounce=10000, keep_alive=True, callback_function=_ActuatorPosition)
+                        debounce=3000, keep_alive=True, callback_function=_ActuatorPosition)
 
-    time.sleep(2.5)
+    time.sleep(.9)
 
     misty.RegisterEvent("DriveEncoders", Events.DriveEncoders, condition=None,
-                        debounce=10000, keep_alive=True, callback_function=_DriveEncoders)
+                        debounce=3000, keep_alive=True, callback_function=_DriveEncoders)
 
-    time.sleep(2.5)
+    time.sleep(.9)
 
     misty.RegisterEvent("IMU", Events.IMU, condition=None,
-                        debounce=10000, keep_alive=True, callback_function=_IMU)
-
-    time.sleep(2.5)
-
-    misty.RegisterEvent("LocomotionCommand", Events.LocomotionCommand,
-                        condition=None, keep_alive=True, callback_function=_LocomotionCommand)
+                        debounce=3000, keep_alive=True, callback_function=_IMU)
 
 
 ################################################################################################
@@ -93,11 +72,18 @@ def proprioception():
 def _FaceRecognition(data):
     print(data)
     if data["message"]["label"] is not ("unknown_person" or None):
-        misty.Speak("Hello ", data["message"]["label"])
+        misty.Speak("Hello {}".format(
+            data["message"]["label"]), None, None, None, True, "tts")
+        misty.StopFaceRecognition()
+        again = input("Search again? y/n ")
+        if again == "n":
+            end()
+        else:
+            print("Continuing")
+            misty.StartFaceRecognition()
 
 
 def facial():
-    mode()
     misty.StartFaceRecognition()
     misty.RegisterEvent("FaceRecognition", Events.FaceRecognition, condition=None,
                         debounce=1000, keep_alive=True, callback_function=_FaceRecognition)
@@ -108,17 +94,23 @@ def facial():
 ################################################################################################
 
 def _TimeOfFlight(data):
-    print(data)
-    # TODO: something interesting here?
+    name = data["message"]["sensorPosition"]
+    d = data["message"]["distanceInMeters"]
+    if data["message"]["status"] == 0:
+        if name in ["Right", "Left", "Center", "Back"] and d < .2:
+            print(name, d)
+        elif d > .06:
+            # pass due to bottom TOF sensor malfunction
+            # print(name, d)
+            pass
 
 
 def tof():
     '''
     The boring half of the visual capabilities.
     '''
-    mode()
-    misty.RegisterEvent("TimeOfFlight", Events.TimeOfFlight, condition=None,
-                        debounce=1000, keep_alive=True, callback_function=_TimeOfFlight)
+    misty.RegisterEvent("TimeOfFlight", Events.TimeOfFlight,
+                        condition=None, keep_alive=True, callback_function=_TimeOfFlight)
 
 
 ################################################################################################
@@ -126,31 +118,33 @@ def tof():
 ################################################################################################
 
 def _BumpSensor(data):
-    name = data["sensorName"]
-    type = data["message"]["isContacted"]
-    print(name, type)
-    name = name[:5]
-    if type:
-        misty.Speak("Bumped ", name)
+    name = data["message"]["sensorId"]
+    hit = data["message"]["isContacted"]
+    print(name, hit)
+    if hit:
+        misty.ChangeLED(255, 0, 0)
+        misty.PlayAudio("A_meow1.mp3", 10)
     else:
-        misty.Speak("Released ", name)
+        misty.ChangeLED(0, 0, 0)
+        misty.PlayAudio("A_meow2.mp3", 10)
 
 
 def _TouchSensor(data):
-    name = data["sensorPosition"]
-    type = data["message"]["isContacted"]
-    print(name, type)
-    if type:
-        misty.Speak("Touched ", name)
+    name = data["message"]["sensorPosition"]
+    hit = data["message"]["isContacted"]
+    print(name, hit)
+    if hit:
+        misty.ChangeLED(0, 255, 0)
+        misty.PlayAudio("A_purr1.mp3", 30)
     else:
-        misty.Speak("Released ", name)
+        misty.ChangeLED(0, 255, 0)
+        misty.StopAudio()
 
 
 def tactile():
     '''
-    Misty talks whenever she's touched. Fun!
+    Misty becomes a cat whenever she's touched. Fun!
     '''
-    mode()
     misty.RegisterEvent("BumpSensor", Events.BumpSensor, condition=None,
                         keep_alive=True, callback_function=_BumpSensor)
     misty.RegisterEvent("TouchSensor", Events.TouchSensor, condition=None,
@@ -178,7 +172,9 @@ def _VoiceRecord(data):
 
 
 def audio():
-    mode()
+    '''
+    TODO: I'm not doing any work on this section until I get some practice with Misty's audio.
+    '''
 
     misty.RegisterEvent("KeyPhraseRecognized", Events.KeyPhraseRecognized, condition=None,
                         debounce=1000, keep_alive=True, callback_function=_KeyPhraseRecognized)
@@ -198,29 +194,35 @@ def audio():
 
 
 def end():
+    misty.UnregisterAllEvents()
     os.system('python3 /Users/skyeworster/Desktop/reset.py')
     print("program ended")
-
-
-def mode():
-    misty.UnregisterAllEvents()
-    misty.StopFaceRecognition()
-    misty.StopSpeaking()
 
 
 if __name__ == "__main__":
     misty = Robot("131.229.41.135")
 
     # clean slate. should print "reset"
-    os.system('python3 /Users/skyeworster/Desktop/reset.py')
-    time.sleep(2)
+    # os.system('python3 /Users/skyeworster/Desktop/reset.py')
+    # time.sleep(2)
 
     # ignore TOF sensors
     misty.UpdateHazardSettings(disableTimeOfFlights=True)
 
-    keyboard.on_press_key("t", tactile())
-    keyboard.on_press_key("p", proprioception())
-    keyboard.on_press_key("f", facial())
-    keyboard.on_press_key("o", tof())
-    keyboard.on_press_key("a", audio())
-    keyboard.on_press_key("e", end())
+    # TODO: add in a = audio
+    print("t = tactile, p = proprioception, f = facial, o = time of flight, e = end")
+    hit = input("Specify mode: ")
+
+    if hit == "t":
+        print("tactile")
+        tactile()
+    elif hit == "p":
+        proprioception()
+    elif hit == "f":
+        facial()
+    elif hit == "o":
+        tof()
+    # elif hit == "a":
+    #     audio()
+    else:
+        end()
