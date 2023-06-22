1e242a99f9d6dde2f071133640d291d1ab104a36
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2023-06-22 10:33:10 -0400

    Update act3imu.py

--- Pablo's Class/Activity 3/act3imu.py
+++ Pablo's Class/Activity 3/act3imu.py
@@ -9,15 +9,12 @@ from mistyPy.Events import Events
 def _BumpSensor(data):
     if data["message"]["isContacted"]:
         misty.ChangeLED(255, 255, 255)
-        misty.StopAudio()
 
 
 def _IMU(data):
-    global neutral
 
-    # print("\n\nIMU\n", data)
     m = data["message"]
-    t = 1  # tolerance
+    t = 2  # tolerance
 
     red = 0
     green = 0
@@ -43,51 +40,8 @@ def _IMU(data):
 
     misty.ChangeLED(red, green, blue)
 
-    t = .2
-    v = 5
-    xa = m["xAcceleration"]
-    if xa > t:
-        misty.PlayAudio("A_kittycat.mp3",volume=v)
-        neutral[3] = False
-    elif xa < -t:
-        misty.PlayAudio("A_Circus.mp3",volume=v)
-        neutral[3] = False
-    else:
-        neutral[3] = True
-
-    ya = m["yAcceleration"]
-    # if ya > t:
-    #     misty.PlayAudio("A_VineBoom.mp3",volume=v)
-    #     neutral[4] = False
-    # elif ya < -t:
-    #     misty.PlayAudio("A_RickrollShort.mp3",volume=v)
-    #     neutral[4] = False
-    # else:
-    #     neutral[4] = True
-
-    za = m["zAcceleration"]
-    # if za > -9.75:
-    #     misty.PlayAudio("A_secrettunnel.mp3",volume=v) # down
-    #     neutral[5] = False
-    # elif za < -9.85:
-    #     misty.PlayAudio("A_CarelessWhisper.mp3",volume=v) # up
-    #     neutral[5] = False
-    # else:
-    #     neutral[5] = True
-
-    print(pv, rv, yv)
-    print("                                                 ", xa, ya, za)
-    # if neutral[0] and neutral[1] and neutral[2]:
-    #     misty.ChangeLED(0, 0, 0)
-    if neutral[3] and neutral[4] and neutral[5]:
-        misty.StopAudio()
-
-    misty.StopAudio()
-
 
 if __name__ == "__main__":
-    global neutral
-    neutral = [True, True, True, True, True, True]  # pv, rv, yv, xa, ya, za
 
     misty = Robot("131.229.41.135")
     misty.RegisterEvent("IMU", Events.IMU, condition=None,
