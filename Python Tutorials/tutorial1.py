ca9bc5ecd77286ddb4056d99884b7ce2bdc4777d
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2023-06-22 15:39:55 -0400

    Update tutorial1.py

--- Python Tutorials/tutorial1.py
+++ Python Tutorials/tutorial1.py
@@ -1,4 +1,14 @@
+'''
+Skye Weaver Worster
+Misty Tutorial 1
+'''
+
+# Import Robot module
 from mistyPy.Robot import Robot
 
-misty = Robot("MISTY-IP-ADDRESS-HERE")
+# Create a Robot object with Misty's IP address
+# We can now send commands to the "misty" object, instead of having to deal with HTTP requests ourselves.
+misty = Robot("MISTY-IP-ADDRESS-HERE") # takes IP address as a string in quotes
+
+# Change Misty's LED to blue (RGB)
 misty.ChangeLED(0,0,255)
