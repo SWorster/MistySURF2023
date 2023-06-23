3091576aff2c6ac0659cd4bf9a375d2b8c10ee76
SWorster <131273379+SWorster@users.noreply.github.com>
2023-06-23 16:59:34 -0400

    changed folder name, and made some little edits to some stuff

--- Pablo's Class/week1demoSWW.py
+++ Pablo's Class/week1demoSWW.py
@@ -1,9 +1,9 @@
 '''
+Skye Weaver Worster
+
 Misty looks around, trying to find my face. If she finds it, she starts following me with her head. If not, she is sad and the program ends.
 
 Target has to be known, and their name must be provided.
-
-This was originally coded to use reset.py. Those lines are commented out.
 '''
 
 # import statements
@@ -11,25 +11,37 @@ from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 from mistyPy.EventFilters import EventFilters
 import time
-# import os
+
+# parameters you can/should change
+misty = Robot("131.229.41.135")  # Misty robot with your IP
+person = "Skye"  # person to look for
+FR_debounce = 500  # facial recognition debounce
+AP_debounce = 100  # actuator position debounce
+volume = 2  # volume for audio clips
+
+# Don't change these! It might break things.
+seen = False  # whether looking at person
+hPitch = None  # tracks head pitch
+hYaw = None  # tracks head yaw
+
+# parameters for Misty's searching movement. The thresholds are a bit less than the maximum values, just in case the head motors don't complete the movement. I don't recommend changing these, except maybe velocity.
+velocity = 50  # speed for movement
+max_left = 81  # left yaw
+left_threshold = 75  # for detecting head placement
+max_right = -81  # right yaw
+right_threshold = -75  # for detecting head placement
+center_yaw = -1  # for last movement command only
+center_threshold = 0.1  # for detecting head placement
+move_pitch = -20  # head pitch
+roll = 0  # head roll (should always be 0)
 
 
 def _BumpSensor(data):
-    '''
-    there's no graceful way to end anything, so...
-    when in doubt, terminate via command line
-    '''
+    # ends program when bumped
     try:
-        print("bumped")
-
-        # should print "reset" to console
-        # os.system('python3 /Users/skyeworster/Desktop/reset.py')
-
-        misty.UnregisterAllEvents()  # just in case
-        misty.StopFaceRecognition()
-
-        # if this doesn't print, pick a god and pray
-        print("end of program")
+        misty.UnregisterAllEvents()  # unregister events
+        misty.StopFaceRecognition()  # stop facial recognition
+        print("end of program")  # confirm program has ended
     except Exception as e:
         print("\nCOULD NOT END PROGRAM", e, "\n")
 
@@ -47,25 +59,21 @@ def _HeadYaw(data):
 
 
 def _Follow(data):
-    '''
-    behavior for when misty has found someone to follow
-    '''
+    # behavior for when misty has found someone to follow
 
     global person
     name = data["message"]["label"]
 
     # sees person of interest
     if name == person:
-        global hYaw
-        global hPitch
-        global seen
+        global hYaw, hPitch, seen
 
+        # get face location
         bearing = data["message"]["bearing"]
         distance = data["message"]["distance"]
         elevation = data["message"]["elevation"]
 
-        # TODO make tolerance relative to distance?
-        # no idea if this works, or if it helps
+        # ? make tolerance relative to distance? no idea if this works, or if it helps
         if distance > 100:
             t = 3
         else:
@@ -73,20 +81,15 @@ def _Follow(data):
 
         # if within tolerance (centered enough in frame)
         if abs(bearing) <= t and abs(elevation) <= t:
-            if seen is False:
-                seen = True
-                misty.ChangeLED(0, 255, 0)
-                misty.PlayAudio("s_Joy4.wav", volume=2)
+            if seen is False:  # if just spotted person
+                seen = True  # record success
+                misty.ChangeLED(0, 255, 0)  # LED green
+                misty.PlayAudio("s_Joy4.wav", volume=volume)  # play audio
 
-        # not within tolerance
-        # TODO put everything below in the else statement?
-        # don't need to move anything
+        # if not within tolerance
         else:
-            misty.ChangeLED(255, 255, 0)
-            seen = False
-
-            # TODO everything below this used to be outside the else statement
-            # see how it works ig
+            misty.ChangeLED(255, 255, 0)  # LED yellow
+            seen = False  # person not centered
 
             y = hYaw  # current yaw
 
@@ -114,7 +117,7 @@ def _Follow(data):
             # if abs(elevation) > t:
             p = hPitch + elevation*e_factor
 
-            misty.MoveHead(pitch=p, yaw=y, duration=.4)
+            misty.MoveHead(pitch=p, yaw=y, duration=.4)  # move head
 
 
 def _FaceRecognition(data):
@@ -124,23 +127,20 @@ def _FaceRecognition(data):
     not sure how timing will work on this. might have to stop her before processing if it takes too long
     '''
 
-    # try to get face name, compare against known faces
-
-    print("FR")
-    try:
-        face = data["message"]["label"]
+    try:  # try to get face name, compare against known faces
+        face = data["message"]["label"]  # face from data
         print(face)
 
         # if known: stop, unregister and reregister to start second phase
         if face == person:
-            misty.Halt()
-            misty.ChangeLED(0, 255, 0)
-            misty.PlayAudio("s_Joy2.wav", volume=2)
+            misty.Halt()  # stop moving
+            misty.ChangeLED(0, 255, 0)  # LED green
+            misty.PlayAudio("s_Joy2.wav", volume=volume)  # play audio
 
-            print("Hello, {}! I'm now following you!".format(face))
+            print(f"Hello, {face}! I'm now following you!")
 
-            misty.UnregisterAllEvents()
-            time.sleep(1)
+            misty.UnregisterAllEvents()  # unregister events
+            time.sleep(1)  # give time to unregister
 
             # register for BumpSensor (halts execution)
             misty.RegisterEvent("BumpSensor", Events.BumpSensor,
@@ -148,15 +148,15 @@ def _FaceRecognition(data):
 
             # register for FR @ 500ms
             # TODO: update comment if this has changed
-            misty.RegisterEvent("FaceRecognition", Events.FaceRecognition,
-                                callback_function=_Follow, keep_alive=True, debounce=500)
+            misty.RegisterEvent("FaceRecognition", Events.FaceRecognition, debounce=FR_debounce, keep_alive=True,
+                                callback_function=_Follow)
 
             # register for HeadPitch/HeadYaw @ 100ms
             misty.RegisterEvent("ActuatorPositionHP", Events.ActuatorPosition, [
-                EventFilters.ActuatorPosition.HeadPitch], debounce=100, keep_alive=True, callback_function=_HeadPitch)
+                EventFilters.ActuatorPosition.HeadPitch], debounce=AP_debounce, keep_alive=True, callback_function=_HeadPitch)
 
             misty.RegisterEvent("ActuatorPositionHY", Events.ActuatorPosition, [
-                EventFilters.ActuatorPosition.HeadYaw], debounce=100, keep_alive=True, callback_function=_HeadYaw)
+                EventFilters.ActuatorPosition.HeadYaw], debounce=AP_debounce, keep_alive=True, callback_function=_HeadYaw)
 
             misty.KeepAlive()  # keep events live
 
@@ -166,55 +166,48 @@ def _FaceRecognition(data):
 
 
 if __name__ == "__main__":
-    misty = Robot("131.229.41.135")  # connect to Misty
     print("Going on an adventure!")
-    misty.UnregisterAllEvents()
-    misty.ChangeLED(0, 0, 255)  # blue
+    misty.UnregisterAllEvents()  # unregister
+    misty.ChangeLED(0, 0, 255)  # LED blue
 
-    global hPitch  # head pitch
-    global hYaw  # head yaw
-    global seen  # if Misty has seen a person
-    seen = False
-    global person  # the person Misty is looking for
-    person = "Skye"
+    misty.MoveHead(-20, 0, 0)  # forward and up for better view
 
-    misty.MoveHead(-20, 0, 0)  # forward and up
-
+    # register for facial recognition
     misty.RegisterEvent("FaceRecognition", Events.FaceRecognition,
                         callback_function=_FaceRecognition, debounce=500, keep_alive=True)
 
+# register for actuator position head pitch
     misty.RegisterEvent("ActuatorPositionHP", Events.ActuatorPosition, [
                         EventFilters.ActuatorPosition.HeadPitch], debounce=50, keep_alive=True, callback_function=_HeadPitch)
 
+# register for actuator position head yaw
     misty.RegisterEvent("ActuatorPositionHY", Events.ActuatorPosition, [
                         EventFilters.ActuatorPosition.HeadYaw], debounce=50, keep_alive=True, callback_function=_HeadYaw)
 
+# register for bump sensor
     misty.RegisterEvent("BumpSensor", Events.BumpSensor,
                         callback_function=_BumpSensor)
 
-    time.sleep(1)
-    misty.StartFaceRecognition()
+    time.sleep(1)  # give time for registration
+    misty.StartFaceRecognition()  # start facial recognition
 
-    misty.MoveHead(-20, 0, -81, 50)  # move to right
-
-    while hYaw > -75:  # wait until fully right
+    # move Misty's head to the right, then left, then return to center. Misty will stop moving when she sees a person
+    misty.MoveHead(move_pitch, roll, max_right, velocity)  # move to right
+    while hYaw > right_threshold:  # wait until fully right
+        pass
+    misty.MoveHead(move_pitch, roll, max_left, velocity)  # move to left
+    while hYaw < left_threshold:  # wait until fully left
+        pass
+    misty.MoveHead(move_pitch, roll, center_threshold,
+                   velocity)  # move to center
+    while hYaw > center_threshold:  # wait until fully centered
         pass
 
-    misty.MoveHead(-20, 0, 81, 50)  # move to left
-
-    while hYaw < 75:  # wait until fully left
-        pass
-
-    misty.MoveHead(-20, 0, -10, 50)  # move to center
-
-    while hYaw > 0.1:  # wait until fully centered
-        pass
-
-    # didn't see target
-    misty.PlayAudio("s_Sadness.wav", volume=2)
-    print("didn't see anyone :(")
-    misty.ChangeLED(255, 0, 0)
-    time.sleep(1)
+    # If we make it to this point, Misty hasn't seen target
+    misty.PlayAudio("s_Sadness.wav", volume=volume)  # play sad noise
+    print("Didn't see anyone :(")
+    misty.ChangeLED(255, 0, 0)  # LED red
+    time.sleep(1)  # wait for audio to finish
 
     # pass a dummy arg to BumpSensor callback, which ends the program
     _BumpSensor(1)
