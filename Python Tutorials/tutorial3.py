985587e67e9b00be827aed27f984d61d829eb3b6
SWorster <131273379+SWorster@users.noreply.github.com>
2023-06-23 11:17:10 -0400

    updated formatting/comments (Streinufication)

--- Python Tutorials/tutorial3.py
+++ Python Tutorials/tutorial3.py
@@ -1,74 +1,65 @@
+'''
+Skye Weaver Worster
+Misty Tutorial #3
+'''
+
 # import statements
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 
-# callback for face training
-def _FaceTraining(data):
-    
-    # if process is complete, unregister from FT event and start recognition
-    try:
-        if data["message"]["isProcessComplete"]:
+
+misty = Robot("131.229.41.135")  # Robot object with your IP
+you = "YourName"  # string with your name.
+
+
+def _FaceTraining(data):  # callback for face training
+    try:  # handles irrelevant/malformed data
+        # if process is complete, unregister from FT event and start recognition
+        if data["message"]["isProcessComplete"]:  # when face training ends
             print("face training complete")
-            misty.UnregisterEvent("FaceTraining")
-            misty.StartFaceRecognition()
-    except:
-        pass
-    
-# callback for all face recognition events
-def _FaceRecognition(data):
-    try:
-        # Use an if statement to check that label does not equal "unknown person" or None. label is included in the message returned by FaceRecognition WebSocket events.
-        name = data["message"]["label"]
-        if (name != "unknown person" and name != None):
-            # If the face is recognized, print a message to greet the person by name.
-            print("A face was recognized. Hello there, " + name + "!");
-            misty.StopFaceRecognition()
-            print("unregistering from all events")
-            misty.UnregisterAllEvents()
+            misty.UnregisterEvent("FaceTraining")  # unregister from FT event
+            misty.StartFaceRecognition()  # begin facial recognition
+    except Exception as e:
+        print(e)
+
+
+def _FaceRecognition(data):  # callback for all face recognition events
+    try:  # handles irrelevant/malformed data
+        name = data["message"]["label"]  # name of person Misty sees
+        if (name != "unknown person" and name != None):  # if person is known/valid
+            print(f"A face was recognized. Hello there, {name}!")
+            misty.StopFaceRecognition()  # ends facial recognition
+            print("Unregistering from all events.")
+            misty.UnregisterAllEvents()  # unregisters from events
             print("program complete")
-    except:
-        pass
+    except Exception as e:
+        print(e)
+
 
 if __name__ == "__main__":
-    misty = Robot("MISTY-IP-ADDRESS-HERE")  # Robot object with your IP
 
-    # Create a global constant called you and assign it to a string with your name.
-    global you
-    you = "YourName"
-
-    # Initialize another variable called onList and set its value to false.
-    global onList
-    onList = False
-    
     # unregister from all events to clear existing facial recognition
     print("unregistering")
     misty.UnregisterAllEvents()
-    
+
     # register for face recognition events
-    misty.RegisterEvent("FaceRecognition", Events.FaceRecognition, callback_function=_FaceRecognition, keep_alive=True)
-    
-    # Store the list of known faces in the faceArr variable and print this list.
-    faceJSON = misty.GetKnownFaces().json()
-    faceArr = faceJSON["result"]
-    print("Learned faces:", faceArr)
-    
-    # onList is True if your name is in the array
-    if you in faceArr:
-        onList = True
-    
-    # Use an if, else statement to execute startFaceRecognition if onList is true and to execute startFaceTraining if otherwise.
-    if onList:
-        print("You were found on the list!")
-        print("starting face recognition")
-        misty.StartFaceRecognition()
-        
-    else:
-        print("You're not on the list...")
-        print("starting face training")
-        
-        # register for FT events
-        misty.RegisterEvent("FaceTraining", Events.FaceTraining, callback_function=_FaceTraining, keep_alive=True)
-        
-        misty.StartFaceTraining(you)
-    
-    misty.KeepAlive()
+    misty.RegisterEvent("FaceRecognition", Events.FaceRecognition,
+                        keep_alive=True, callback_function=_FaceRecognition)
+
+    # Store the list of known faces and print it
+    faceJSON = misty.GetKnownFaces().json()  # get list of known faces as JSON
+    face_array = faceJSON["result"]  # convert to array
+    print("Learned faces:", face_array)  # print to console
+
+    if you in face_array:  # if your name is in the array
+        print("You were found on the list! Starting face recognition...")
+        misty.StartFaceRecognition()  # starts facial recognition
+
+    else:  # if your name is not in the array
+        print("You're not on the list. Starting face training...")
+
+        # register for face training events
+        misty.RegisterEvent("FaceTraining", Events.FaceTraining,
+                            keep_alive=True, callback_function=_FaceTraining)
+
+        misty.StartFaceTraining(you)  # start face training
