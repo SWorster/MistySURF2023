85d20b07abeb24a97672a58fe9d21f5eaedd1623
SWorster <131273379+SWorster@users.noreply.github.com>
2023-06-29 10:25:45 -0400

    made sensing folder

--- Pablo's Class/3: Sensing/act3audio.py
+++ Pablo's Class/3: Sensing/act3audio.py
@@ -24,10 +24,10 @@ import time
 misty = Robot("131.229.41.135")  # robot object
 STDM_debounce = 100  # SourceTrackDataMessage debounce, in ms
 t = 3  # listening time, in seconds
-v = 2 # volume
+v = 2  # volume
 
 # ! DO NOT CHANGE
-count = -1 # starting number of claps. first isn't counted, because Misty picks up her own fan noise for a bit
+count = -1  # starting number of claps. first isn't counted, because Misty picks up her own fan noise for a bit
 
 
 def _SourceTrackDataMessage(data):
@@ -50,15 +50,17 @@ def _SourceTrackDataMessage(data):
         place = sectors.index(max(sectors))  # find where loudest avg sound is
         if place == 0:  # front
             misty.ChangeLED(255, 0, 0)  # red
+            misty.DriveTime(50, 0, 3)  # drive forward
         elif place == 1:  # right
             misty.ChangeLED(255, 200, 0)  # yellow
+            misty.DriveTime(0, , 3)  # turn right
         elif place == 2:  # back
             misty.ChangeLED(0, 255, 0)  # green
         elif place == 3:  # left
             misty.ChangeLED(0, 0, 255)  # blue
 
         # TODO: I made this test specific to the front because I'm currently sitting in front of Misty. I'll make this more general once I iron out everything
-        if front > 30: # if loud sound detected in front of Misty
+        if front > 30:  # if loud sound detected in front of Misty
             count += 1
             print(f"                         {count}")
 
@@ -70,29 +72,29 @@ if __name__ == "__main__":
     misty.StopAudio()
 
     misty.StartRecordingAudio("test")  # start listening
-    
-    time.sleep(1) # give time for fans to stop
-    
+
+    time.sleep(1)  # give time for fans to stop
+
     # register for SourceTrackDataMessage
     misty.RegisterEvent("SourceTrackDataMessage", Events.SourceTrackDataMessage, condition=None,
                         debounce=STDM_debounce, keep_alive=True, callback_function=_SourceTrackDataMessage)
-    
+
     time.sleep(t)  # time for listening
     misty.StopRecordingAudio()  # stop listening
     misty.UnregisterAllEvents()  # unregister
-    misty.ChangeLED(0,0,0)
-    
+    misty.ChangeLED(0, 0, 0)
+
     # play different audio clip depending on how many claps detected
     if count == 0:
-        misty.PlayAudio("A_Lacrimosa.mp3",v*3)
+        misty.PlayAudio("A_Lacrimosa.mp3", v*3)
     elif count == 1:
-        misty.PlayAudio("A_RickrollShort.mp3",v)
+        misty.PlayAudio("A_RickrollShort.mp3", v)
     elif count == 2:
-        misty.PlayAudio("A_CarelessWhisper.mp3",v)
+        misty.PlayAudio("A_CarelessWhisper.mp3", v)
     elif count == 3:
-        misty.PlayAudio("A_Circus.mp3",v)
+        misty.PlayAudio("A_Circus.mp3", v)
     elif count == 4:
-        misty.PlayAudio("A_megalovania.m4a",v)
-    
-    time.sleep(2) # stop audio after a while
+        misty.PlayAudio("A_megalovania.m4a", v)
+
+    time.sleep(2)  # stop audio after a while
     misty.StopAudio()
