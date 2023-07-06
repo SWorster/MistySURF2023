c4546c8006db4a4481dcdcb514a45a09e0e04a0b
SWorster <131273379+SWorster@users.noreply.github.com>
2023-07-06 10:26:06 -0400

    finished wall following

--- Pablo's Class/3: Sensing/bumpWallFollow.py
+++ Pablo's Class/3: Sensing/bumpWallFollow.py
@@ -1,118 +1,96 @@
 '''
 Skye Weaver Worster
 
-WORK IN PROGRESS: still having some issues. Misty sometimes bumps into the wall and doesn't back up at all.
-
-Misty drives until bumped (stop and change light depending on bumper touched). She does not resume moving after contact.
+Misty uses her bump sensors to follow a wall. She halts when more than one bumper is pressed.
 
 WARNING: This code temporarily disables Misty's TOF sensor hazards, so she won't automatically stop at table edges and other drops. Be careful!
 '''
 
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
-import time
 
 misty = Robot("131.229.41.135")  # robot with your IP
 volume = 2  # volume for audio
-lin_vel = 10  # linear velocity
-ang_vel = 80  # angular velocity
-d_time = 1500  # drive time for bump, in milliseconds
+lin_vel = 10  # linear velocity for driving forward
+lin_turn = 5 # linear velocity for backing up turn. Don't set this too low!
+ang_vel = 80  # angular velocity for backing up turn
+d_time = 1500  # drive time for backing up turn, in milliseconds
 DE_debounce = 100  # drive encoders debounce
 min_speed = 1  # the minimum speed at which Misty is still considered to be "driving"
 
 # ! Do not change these!
-contact = [False, False, False, False]  # which sensors are contacted
-back_up = False  # tracks whether Misty is backing up
-free = True  # tracks whether Misty is driving forward
-
+current = None  # which bumper we're using for turning in place
+backup = False  #  whether Misty is in backup mode
+driving = False # whether Misty is moving while in backup mode
 
 def _DriveEncoders(data):
-    global back_up, contact, lin_vel, free
+    global backup, lin_vel, min_speed, driving
 
     try:
+        if current != None:  # if touching something
+            backup = True  # start backup mode
 
-        # get velocity from data
-        left_vel = abs(data["message"]["leftVelocity"])
-        right_vel = abs(data["message"]["rightVelocity"])
-        vel = left_vel+right_vel  # total velocity
+        if backup:  # if in backup mode (regardless of whether still in contact)
+            left_vel = abs(data["message"]["leftVelocity"])
+            right_vel = abs(data["message"]["rightVelocity"])
+            vel = left_vel+right_vel  # total velocity
+            
+            if vel > min_speed and not driving: # if moving and haven't flipped driving yet
+                driving = True # have started driving
+            elif vel <  min_speed and not driving: # if haven't started driving yet
+                pass # wait for treads to start moving
+            elif vel < min_speed and driving:  # if not moving anymore (DriveTime ended)
+                backup = False  # end backup mode
+                driving = False # no longer driving
+                misty.Drive(lin_vel, 0)  # go forward
 
-        # governs behavior when backing up
-        if not free:  # doing back-up movement
-            if vel > min_speed:  # if moving
-                if not back_up:  # first time seeing movement
-                    print("back_up=true")
-                    back_up = True  # misty has just started moving back
-                else:  # misty has already been moving
-                    pass  # do nothing
-            else:  # misty not moving
-                if back_up:  # if misty has completed the drive
-                    print("free=true")
-                    back_up = False  # no longer driving backwards
-                    free = True  # now moving freely again
-                    misty.Drive(lin_vel, 0)  # go forward
-                else:  # misty has not begun driving
-                    pass  # do nothing
-
-        print(back_up, free)
+        print(" backup", backup, "driving", driving)
 
     except Exception as e:
         print("DriveEncoders error:", e)
 
 
 def _BumpSensor(data):
-    global contact, free
+    global current, lin_vel, lin_turn, ang_vel, d_time, volume
 
     if data["message"]["isContacted"] == True:  # if Misty hits something
-        # misty.Stop()  # stop moving (should happen automatically)
-        free = False  # no longer moving freely
+        misty.Stop()  # stop moving (should happen automatically)
 
         name = data["message"]["sensorId"]  # get name
-        print(name)
 
-        # record which bumper was hit
-        if name == "bfr":  # front right
-            misty.ChangeLED(255, 0, 0)  # red
-            contact[0] = True
-        elif name == "bfl":  # front left
-            misty.ChangeLED(0, 255, 0)  # green
-            contact[1] = True
-        elif name == "brr":  # back right
-            misty.ChangeLED(0, 0, 255)  # blue
-            contact[2] = True
-        elif name == "brl":  # back left
-            misty.ChangeLED(255, 160, 0)  # yellow
-            contact[3] = True
-
-        if sum(contact) > 1:  # multiple contacts, end program
-            misty.Stop()
+        if current != None:  # if multiple bumpers hit, end program
+            misty.Stop()  # stop moving
             misty.ChangeLED(0, 0, 0)  # LED off
             misty.PlayAudio("meow1.mp3", volume=volume)
             misty.UnregisterAllEvents()  # unregister and reset hazards
             misty.UpdateHazardSettings(revertToDefault=True)
 
-        elif sum(contact) == 1:  # if only one contact
+        else: # if only one bumper hit
+            current = name  # set current
+
+            # behavior for each bumper
             if name == "bfr":  # front right
-                misty.DriveTime(-lin_vel, ang_vel, d_time)  # go back right
-            if name == "bfl":  # front left
-                misty.DriveTime(-lin_vel, -ang_vel, d_time)  # go back left
-            if name == "brr":  # back right
-                misty.DriveTime(lin_vel, ang_vel, d_time)  # go front right
-            if name == "brl":  # back left
-                misty.DriveTime(lin_vel, -ang_vel, d_time)  # go front left
+                misty.ChangeLED(255, 0, 0)  # red
+                misty.DriveTime(-lin_turn, ang_vel, d_time)  # go back right
+            elif name == "bfl":  # front left
+                misty.ChangeLED(0, 255, 0)  # green
+                misty.DriveTime(-lin_turn, -ang_vel, d_time)  # go back left
+            elif name == "brr":  # back right
+                misty.ChangeLED(0, 0, 255)  # blue
+                misty.DriveTime(lin_turn, ang_vel, d_time)  # go front right
+            elif name == "brl":  # back left
+                misty.ChangeLED(255, 160, 0)  # yellow
+                misty.DriveTime(lin_turn, -ang_vel, d_time)  # go front left
 
     else:  # if data["message"]["isContacted"] == False:  # if sensor released
-        name = data["message"]["sensorId"]  # get name
         misty.ChangeLED(0, 0, 0)  # LED off
-        if name == "bfr":  # front right
-            contact[0] = False
-        if name == "bfl":  # front left
-            contact[1] = False
-        if name == "brr":  # back right
-            contact[2] = False
-        if name == "brl":  # back left
-            contact[3] = False
 
-    print(contact)
+        name = data["message"]["sensorId"]  # get name
+        if name == current:
+            # free = True
+            current = None
+
+    print("current", current)
 
 
 if __name__ == "__main__":
