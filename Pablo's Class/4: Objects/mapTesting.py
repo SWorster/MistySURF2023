bc80e12962260684e84760b8085f169017600f96
SWorster <131273379+SWorster@users.noreply.github.com>
2023-07-13 11:36:00 -0400

    map work

--- Pablo's Class/4: Objects/mapTesting.py
+++ Pablo's Class/4: Objects/mapTesting.py
@@ -16,6 +16,7 @@ import math
 
 misty = Robot("131.229.41.135")  # robot with your IP
 map_name = "polish dancing cow"  # name of map to plot on top of
+img_name = "polish_dancing_cow.png"  # name to save image as
 
 obj1 = "book"  # object to the left
 obj2 = "bottle"  # object to the right
@@ -60,7 +61,6 @@ else:
 
 # get current map data
 arr = np.array(misty.GetMap().json()["result"]["grid"])
-size = arr.shape[0]
 for row in range(arr.shape[0]):
     for col in range(arr.shape[1]):
         if arr[row][col] == 1:  # open = 1
@@ -75,49 +75,78 @@ arr = arr.astype(np.uint8)
 data = im.fromarray(arr)
 # originally when created is upside down in comparison to studio's image, so need to rotate it
 data = data.rotate(180)
-data.save(map_name, format="PNG")
+data.save(img_name, format="PNG")
 
-img = plt.imread(map_name)
+img = plt.imread(img_name)
 fig, ax = plt.subplots()
-ax.imshow(img, extent=[0, size, 0, size], cmap='gray')
+ax.imshow(img, extent=[0, data.size[0], 0, data.size[1]], cmap='gray')
+ax.set_xlim(0, data.size[0])
+ax.set_ylim(0, data.size[1])
 
 
-# metersPerCell is the area in m^2 covered by each cell.
-# cell length is therefore sqrt(mPC).
-# converting from cell to meters is c_d / scale
-# converting from meters to cell is m_d * scale
-scale = math.sqrt(misty.GetMap().json()["result"]["metersPerCell"])
 
-start_yaw = 180  # initial yaw
+'''
+CALCULATING DRIVING PATH
+'''
+
+
+start_yaw = 10  # initial yaw
 yaw1 = 100  # yaw of first object center
 yaw2 = 40  # yaw of second object center
 actual_middle = (yaw1+yaw2)/2  # actual yaw at middle (wherever Misty ended up)
 d_meter = 1  # thru driving distance, in meters
 
-
 # TODO get starting position in terms of map orientation
-# seems like position is values from 0-1 that scale how many pixels are in a meter. not something i can deal with yet
-x1 = 100  # starting x
-y1 = 100  # starting y
+# seems like position is values from 0-1 that scale how many pixels are in a meter. not something i want to deal with yet
+# cell position, not meters
+x1 = 150  # starting x
+y1 = 250  # starting y
 
-d_cell = scale * d_meter  # distance in cell, from meters
+# metersPerCell is the area in m^2 covered by each cell.
+# cell length is therefore sqrt(mPC).
+# converting from cell to meters is c_d / scale
+# converting from meters to cell is m_d * scale
+mpc = misty.GetMap().json()["result"]["metersPerCell"]
+print(mpc)
+scale = math.sqrt(mpc)
+print(scale)
+
+
+d_cell = scale * d_meter  # distance driven in cells, from meters
 
 # ? Is map oriented with initial yaw = 0? for now i'm assuming that Misty's 0 yaw is along the x axis. I can adjust this later if needed.
+
+# x = r cos theta
+# y = r sin theta
 
 # calculate and plot driving path
 x2 = x1 + d_cell * math.cos(actual_middle)
 y2 = y1 + d_cell * math.sin(actual_middle)
-plt.plot([x1, x2], [y1, y2], 'ro-')
+plt.plot([x1, x2], [y1, y2], 'r.-', label="movement")
 
 # calculate and plot first object angle
 x2 = x1 + d_cell * math.cos(yaw1)
 y2 = y1 + d_cell * math.sin(yaw1)
-plt.plot([x1, x2], [y1, y2], 'yo-')
+plt.plot([x1, x2], [y1, y2], 'g.-', label=obj1)
 
 # calculate and plot second object angle
 x2 = x1 + d_cell * math.cos(yaw2)
 y2 = y1 + d_cell * math.sin(yaw2)
-plt.plot([x1, x2], [y1, y2], 'bo-')
+plt.plot([x1, x2], [y1, y2], 'b.-', label=obj2)
 
-plt.legend("Movement", f"{obj1}", f"{obj2}")
+plt.legend(loc="lower left")
+
+'''
+GET MISTY'S CURRENT LOCATION
+'''
+
+
+
+# i'll do this later lol
+
+
+
+
+
+
 plt.show()
