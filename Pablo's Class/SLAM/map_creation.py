ed3969e5c99a31b85ce8ee78f97151f0b5e58808
Julia Yu <jyu@smith.edu>
2023-07-17 15:27:47 -0400

    Update map_creation.py

--- Pablo's Class/SLAM/map_creation.py
+++ Pablo's Class/SLAM/map_creation.py
@@ -4,10 +4,9 @@ import serial
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 
-MISTY_IP = "<Replace w/ Misty's IP address>"
-ARDUINO_PORT = "<Replace w/ COM port of the Arduino>"
-
-misty = Robot(MISTY_IP) # create a Misty instance using its IP address (which varies from robot to robot)
+'Place the following information into the 2 constants below'
+MISTY_IP = "<Misty's IP>"
+ARDUINO_PORT = "Arduino COM"
 
 'The 4 constants below are thresholds for the joystick position to move in different directions'
 NORTH = 341 # North and South are in Y
@@ -17,11 +16,13 @@ RIGHT = 682
 
 'Globals for callback reference'
 startMovement = False # this will prevent Misty from moving until she has a pose
-startedMapping = False
-lostStartTime = datetime.now()
-hasPose = False
+startedMapping = False # keeps track of if Misty has started to map
+lostStartTime = datetime.now() # gets the current time
+hasPose = False # tracks if Misty has a pose and knows where she is
 
-def _SlamData(data):
+misty = Robot(MISTY_IP) # create a Misty instance using its IP address (which varies from robot to robot)
+
+def _SlamData(data): # callback for whenever the slam statuses change
     global startMovement, startedMapping, lostStartTime, hasPose
     status = data["message"]["slamStatus"]["statusList"] # gets and stores the list of current statuses of the slam system
     print(data["message"]["created"], status)
@@ -32,7 +33,7 @@ def _SlamData(data):
         hasPose = True # flags that she has a pose
     elif ("HasPose" not in status) and (not startedMapping): # make the LED red if she lost her pose or mapping isn't ready yet
         misty.ChangeLED(255, 0, 0)
-    elif startedMapping == True and "LostPose" in status:
+    elif startedMapping == True and "LostPose" in status: # if Misty started mapping (meaning she got a pose) and then she loses the pose
         lostStartTime = datetime.now() # records the current time that the robot lost pose
         hasPose = False
         misty.ChangeLED(255, 0, 0)
@@ -42,7 +43,7 @@ def treads(coords): # Controls the treads for overall mobility
     split = coords.split() # data format: [x, y, mode]
     x = int(split[0])
     y = int(split[1])
-    if startMovement:
+    if startMovement: # only lets Misty move once she gets a pose
         if y < NORTH and x > LEFT and x < RIGHT: # move forward (hold up on joystick)
             misty.Drive(linearVelocity = 20, angularVelocity = 0)
         elif y > SOUTH and x > LEFT and x < RIGHT: # move backward (hold down on joystick)
@@ -67,7 +68,7 @@ def mode(data): # Switches the mode being used depending on the button pressed
     command = int(split[2])
     if command == 1:
         treads(data)
-    elif command == 2:
+    elif command == 2: # passes over 2 and 3 (they'd do nothing if pressed)
         pass
     elif command == 3:
         pass
@@ -82,8 +83,7 @@ def init(): # Resets Misty's head position and LED color. Disables the hazard To
 if __name__ == "__main__":
     init()
     ser = serial.Serial(ARDUINO_PORT, 9600, timeout = 1) # open connection to the COM port that the arduino is connected to to get serial data from it
-    # create an event listener for Misty's slam status; callbeck is used whenever there is an update to the status
-    misty.RegisterEvent(event_name = "stats", event_type = Events.SlamStatus, callback_function = _SlamData, keep_alive = True)
+    misty.RegisterEvent(event_name = "stats", event_type = Events.SlamStatus, callback_function = _SlamData, keep_alive = True) # create an event listener for Misty's slam status
     misty.StartMapping() # starts the mapping process
     while True:
         line = ser.readline() # get next line of the serial monitor (its in bytes)
